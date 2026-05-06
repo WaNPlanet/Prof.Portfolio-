92b4be86a2e5e8b2f93b528132743ba72b6f0b3b
WaNPlanet <110819328+WaNPlanet@users.noreply.github.com>
2026-05-06 03:38:08 +0000

    youtube link

--- app/components/socials/page.tsx
+++ app/components/socials/page.tsx
@@ -4,7 +4,7 @@ import { FaLinkedinIn, FaYoutube , FaGithub } from "react-icons/fa";
 export default function SocialIcons() {
   return (
     <div className="flex gap-4 text-gray-600 text-sm md:text-base">
-      <a href="https://www.youtube.com/@Planet.speaks" target="_blank" aria-label="YouTube" className="hover:text-black">
+      <a href="https://www.youtube.com/@TheCorporation_Stack" target="_blank" aria-label="YouTube" className="hover:text-black">
         <FaYoutube className="h-5 w-5" />
       </a>
       <a href="https://www.linkedin.com/in/ofosuhene-kyere-267076246" target="_blank" aria-label="LinkedIn" className="hover:text-black">
